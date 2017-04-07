4f59c1e7d25f24418cb18cfdf1c0e63d7a201c31
Lucas Pains <jlucaspains@gmail.com>
2017-04-06 18:16:14 -0600

    Parse text to object using Regex and Reflection

---
+++ CSharp/util/LPains.Utilities.Tests/TextToObjectWithRegexExtensionFixture.cs
@@ -0,0 +1,39 @@
+﻿
+using Xunit;
+using LPains.Utilities;
+
+namespace LPains.Utilities.Tests
+{
+    public class ProductPrice
+    {
+        public string ProductCode { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class TextToObjectWithRegexExtensionFixture
+    {
+        [Fact]
+        public void Test()
+        {
+            var result = "123456 . 22.2".ParseToObject<ProductPrice>("(?<ProductCode>\\d{6}) \\. (?<Price>\\d{2}\\.\\d)");
+            Assert.NotNull(result);
+            Assert.Equal("123456", result.ProductCode);
+            Assert.Equal(22.2M, result.Price);
+
+            var result2 = "65432133.3".ParseToObject<ProductPrice>("(?<ProductCode>\\d{6})(?<Price>\\d{2}\\.\\d)");
+            Assert.NotNull(result);
+            Assert.Equal("654321", result2.ProductCode);
+            Assert.Equal(33.3M, result2.Price);
+
+            var result3 = "444.4524142".ParseToObject<ProductPrice>("(?<Price>\\d{3}\\.\\d)(?<ProductCode>\\d{6})");
+            Assert.NotNull(result);
+            Assert.Equal("524142", result3.ProductCode);
+            Assert.Equal(444.4M, result3.Price);
+
+            var result4 = "555.5".ParseToObject<ProductPrice>("(?<Price>\\d{3}\\.\\d)");
+            Assert.NotNull(result);
+            Assert.Null(result4.ProductCode);
+            Assert.Equal(555.5M, result4.Price);
+        }
+    }
+}
---
+++ CSharp/util/LPains.Utilities/TextToObjectWithRegexExtension.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace LPains.Utilities
+{
+    /// <summary>
+    /// Provides a extension method for transforming string to object using a regular expression as parser
+    /// </summary>
+    public static class TextToObjectWithRegexExtension
+    {
+        /// <summary>
+        /// Creates an instance of <typeparamref name="TData"/> and assigns its properties based on captures from regular expression in <paramref name="regularExpression"/>
+        /// </summary>
+        /// <typeparam name="TData">Result type. Must be newable.</typeparam>
+        /// <param name="data">string to be parsed</param>
+        /// <param name="regularExpression">regular expression with named group captures used to parse <paramref name="data"/> into <typeparamref name="TData"/></param>
+        /// <returns></returns>
+       public static TData ParseToObject<TData>(this string data, string regularExpression) where TData: new()
+        {
+            if (string.IsNullOrEmpty(data)) throw new ArgumentNullException(nameof(data));
+            if (string.IsNullOrEmpty(regularExpression)) throw new ArgumentNullException(nameof(regularExpression));
+
+            var result = new TData();
+            var properties = result.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var groups = Regex.Match(data, regularExpression).Groups;
+
+            foreach (var propertyToSet in properties)
+            {
+                if (propertyToSet.Name.ToUpper(CultureInfo.InvariantCulture) == "RAWINPUT")
+                {
+                    propertyToSet.SetValue(result, data, null);
+                    continue;
+                }
+
+                var group = groups[propertyToSet.Name];
+                var propertyValue = group.Success ? group.Value : null;
+                if ((!propertyToSet.PropertyType.IsValueType || string.IsNullOrEmpty(propertyValue)) && propertyToSet.PropertyType != typeof(string)) continue;
+
+                var value = propertyToSet.PropertyType.IsEnum
+                    ? Enum.Parse(propertyToSet.PropertyType, propertyValue)
+                    : Convert.ChangeType(propertyValue, propertyToSet.PropertyType, CultureInfo.InvariantCulture);
+
+                propertyToSet.SetValue(result, value, null);
+            }
+
+            return result;
+        }
+    }
+}
